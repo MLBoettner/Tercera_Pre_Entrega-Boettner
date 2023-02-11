d4cbfc741b5ad24d0438cc0163296a0302d65b4e
Luisa Boettner <luisa7284@hotmail.com>
2023-02-11 18:30:50 -0300

    Inicializacion Repositorio

---
+++ AppCoder/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Especialidad (models.Model):
+    
+    nombre=models.CharField(max_length=40)
+    # Grupo etario  1 (ninos) 2 (adolecentes) 3 (adultos) 4 (adultos mayores)
+    grupoetario=models.IntegerField()
+
+
+class Paciente (models.Model):
+
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    email=models.EmailField()
+
+class Medico (models.Model):
+
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    email=models.EmailField()
+    profesion=models.CharField(max_length=30)
+
+class Estudio (models.Model):
+    nombre=models.CharField(max_length=30)
+    fecha=models.DateField()
+    entregado=models.BooleanField()
+
